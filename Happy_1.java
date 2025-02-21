ef76a5c3a2b6cb7fdba3d93723e9f21fe2e6ee5c
happy0524 <jhcha524@gmail.com>
2025-02-21 16:58:23 +0900

    Add files via upload

---
+++ Happy_1.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Happy_1 {
+    public static void main(String[] arg) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextLine());
+        System.out.println(scan.next());
+        scan.close();
+    }
+}
