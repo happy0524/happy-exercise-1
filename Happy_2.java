aece4f85dcd4582703b789b1086eedbb463af67e
happy0524 <jhcha524@gmail.com>
2025-02-23 23:23:39 +0900

    Variable initialization
    initialize variables and printed it.

---
+++ Happy_2.java
@@ -0,0 +1,9 @@
+public class Happy_2 {
+    public static void main(String[] args) {
+        int a;
+        int b = 10;
+        a = 11;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
