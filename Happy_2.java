494893aa868275dfde1734f5e0ca2cf3c627830e
happy0524 <jhcha524@gmail.com>
2025-02-24 01:47:30 +0900

    More exercise about Variables
    1. Alignment on printf()
    2. Using format specifier 'var'

--- Happy_2.java
+++ Happy_2.java
@@ -1,9 +1,21 @@
 public class Happy_2 {
     public static void main(String[] args) {
         int a;
-        int b = 10;
+        int b = 11111;
         a = 11;
+        char gg = 'k';
+        boolean isTrue = true;
+        var d = 'h';
+        double kia = 7.1234;
         System.out.println(a);
         System.out.println(b);
+        System.out.println(d);
+        System.out.printf("%c\n" , gg);
+        System.out.printf("%b\n" , isTrue);
+        System.out.printf("%7d\n" , b);
+        System.out.printf("%-7d\n" , b);
+        System.out.printf("%07d\n" , b);
+        System.out.printf("%,7d\n" , b);
+        System.out.printf("%f\n" , kia);
     }
 }
