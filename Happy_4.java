e56c548409e8982c576797342cbd970818e1d82f
happy0524 <jhcha524@gmail.com>
2025-02-28 02:10:14 +0900

    Type Conversion
    Practiced Type Conversion

---
+++ Happy_4.java
@@ -0,0 +1,8 @@
+public class Happy_4 {
+    public static void main(String[] args){
+        int a = 100;
+        int b = 3;
+        double div = (double)a / (double)b; // 강제 형변환
+        System.out.println(div); // 출력
+    }
+}
