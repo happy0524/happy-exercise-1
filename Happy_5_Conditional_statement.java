7cff0f07c6561c5cced7fe27a8ee2ae9010aabfb
happy0524 <jhcha524@gmail.com>
2025-03-01 03:31:04 +0900

    Conditional_statement
    Practice about conditional statement

---
+++ Happy_5_Conditional_statement.java
@@ -0,0 +1,10 @@
+public class Happy_5_Conditional_statement {
+    public static void main(String[] args) {
+        int score = 83;
+        if (score >= 90) {
+            System.out.println("Grade : A");
+        } else {
+            System.out.println("Grade : NOT A");
+        }
+    }
+}
