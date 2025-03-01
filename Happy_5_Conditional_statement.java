0573e22635f50c72b207dc0650c18f7981396af0
happy0524 <jhcha524@gmail.com>
2025-03-01 14:35:00 +0900

    Conditional_statement 2
    nested ver

--- Happy_5_Conditional_statement.java
+++ Happy_5_Conditional_statement.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Happy_5_Conditional_statement {
     public static void main(String[] args) {
-        int score = 83;
-        if (score >= 90) {
-            System.out.println("Grade : A");
-        } else {
-            System.out.println("Grade : NOT A");
+        System.out.println("학년을 입혁하세요.");
+        Scanner scanGrade = new Scanner(System.in);
+        int grade = scanGrade.nextInt();
+        if (grade == 4) {
+            System.out.println("점수를 입력하세요.");
+            Scanner scanScore = new Scanner(System.in);
+            int score = scanScore.nextInt();
+            if (score >= 90) {
+                System.out.println("Grade : A");
+            } else if (80 <= score && score < 90) {
+                System.out.println("Grade : B");
+            }
         }
     }
 }
