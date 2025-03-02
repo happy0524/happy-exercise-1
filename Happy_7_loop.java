0d67c462350c1617d641829f5073647825b0037d
happy0524 <jhcha524@gmail.com>
2025-03-02 16:49:56 +0900

    loop practice
    구구단

---
+++ Happy_7_loop.java
@@ -0,0 +1,9 @@
+public class Happy_7_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <=9; i++) {
+            for (int k = 1; k<=9; k++) {
+                System.out.println(i + "X" + k + "=" + i*k);
+            }
+        }
+    }
+}
