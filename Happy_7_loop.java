13709f19dd5afc7e491438213ace15c69c21401e
happy0524 <jhcha524@gmail.com>
2025-03-02 16:56:55 +0900

    loop practice2
    질문 받는 구구단

--- Happy_7_loop.java
+++ Happy_7_loop.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Happy_7_loop {
     public static void main(String[] args) {
-        for (int i = 1; i <=9; i++) {
-            for (int k = 1; k<=9; k++) {
-                System.out.println(i + "X" + k + "=" + i*k);
-            }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("몇 단을 출력할까요? :");
+        int dan = scan.nextInt();
+        for (int k = 1; k<=9; k++) {
+                System.out.println(dan + "X" + k + "=" + dan*k);
         }
     }
 }
