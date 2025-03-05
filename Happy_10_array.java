5f6f88e1a6941c576c113a2bf470de26cb0b63f2
happy0524 <jhcha524@gmail.com>
2025-03-05 19:16:48 +0900

    array
    practice array

---
+++ Happy_10_array.java
@@ -0,0 +1,13 @@
+public class Happy_10_array {
+    public static void main(String[] args) {
+
+        int [] array = {23,24,30,35,42};
+        // array = new int [10];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(array[i]);
+        }
+
+
+    }
+}
